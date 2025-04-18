f3d671b0e30c1592827065c1f3a178a758ea847b
MarineP <marine.porret@gmail.com>
2025-04-18 17:16:17 +0200

    ✅ Add tests for user service

--- front/src/app/services/user.service.spec.ts
+++ front/src/app/services/user.service.spec.ts
@@ -1,22 +1,55 @@
-import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { UserService } from './user.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpMock: HttpTestingController;
+
+  const userMock = {
+    token: "token",
+    type: "type",
+    id: 42,
+    username: "UserTes",
+    firstName: "User",
+    lastName: "Test",
+    admin: false,
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports:[
-        HttpClientModule
+        HttpClientTestingModule
       ]
     });
     service = TestBed.inject(UserService);
+    httpMock = TestBed.inject(HttpTestingController)
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  it('should get user', (done) => {
+    service.getById('42').subscribe((user) => {
+      expect(user).toBe(userMock)
+      done()
+    })
+
+    const req = httpMock.expectOne({
+      method: 'GET',
+      url: 'api/user/42'
+    })
+    req.flush(userMock)
+  })
+
+  it('should delete user', (done) => {
+    service.delete('42').subscribe(() => {
+      done()
+    })
+
+    const req = httpMock.expectOne({
+      method: 'DELETE',
+      url: 'api/user/42'
+    })
+    req.flush({})
+  })
 });
