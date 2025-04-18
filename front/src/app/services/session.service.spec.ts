c742952e540b6aa9835ca4b2641b1f805ee166ad
MarineP <marine.porret@gmail.com>
2025-04-18 16:20:40 +0200

    ✅ Add tests for session service

--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -6,12 +6,38 @@ import { SessionService } from './session.service';
 describe('SessionService', () => {
   let service: SessionService;
 
+  const userMock = {
+    token: "token",
+    type: "type",
+    id: 42,
+    username: "UserTes",
+    firstName: "User",
+    lastName: "Test",
+    admin: false,
+  }
+
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(SessionService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  it('should log in and update sessionInformation', () => {
+    service.logIn(userMock)
+    expect(service.isLogged).toBe(true)
+    expect(service.sessionInformation).toBe(userMock)
+
+    service.$isLogged().subscribe((value) => {
+      expect(value).toBe(true);
+    })
+  })
+
+  it('should log out and reset sessionInformation', () => {
+    service.logOut()
+    expect(service.isLogged).toBe(false)
+    expect(service.sessionInformation).toBeUndefined()
+
+    service.$isLogged().subscribe((value) => {
+      expect(value).toBe(false);
+    })
+  })
 });
