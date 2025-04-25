bfc6cb119cfc3d5b1fb39e4ba07907f41e52c2db
MarineP <marine.porret@gmail.com>
2025-04-25 11:48:52 +0200

    ✅ Add e2e tests for register feature

---
+++ front/cypress/e2e/register.cy.ts
@@ -0,0 +1,62 @@
+/// <reference types="cypress" />
+
+describe('Register spec', () => {
+    beforeEach(() => {
+      cy.visit('/register')
+  
+      cy.intercept(
+        {
+          method: 'GET',
+          url: '/api/session',
+        },
+        []).as('session')
+    })
+  
+    it('Register successfull', () => {
+      cy.intercept('POST', '/api/auth/register', {
+        body: {
+          id: 1,
+          username: 'userName',
+          firstName: 'firstName',
+          lastName: 'lastName',
+          admin: true
+        },
+      })
+  
+      cy.get('input[formControlName=firstName]').type("User")
+      cy.get('input[formControlName=lastName]').type("Test")
+      cy.get('input[formControlName=email]').type("user@test.com")
+      cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+  
+      cy.url().should('include', '/login')
+    })
+  
+    it('Register failed, email already exist', () => {
+      cy.intercept('POST', '/api/auth/register', {
+        statusCode: 409,
+      })
+  
+      cy.get('input[formControlName=firstName]').type("User")
+      cy.get('input[formControlName=lastName]').type("Test")
+      cy.get('input[formControlName=email]').type("user@test.com")
+      cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+  
+      cy.get('[data-testid=register-error]').should('exist')
+    })
+  
+    it('Mandatory field missing', () => {
+      cy.get('input[formControlName=firstName]').type("User")
+      cy.get('input[formControlName=lastName]').type("Test")
+      cy.get('input[formControlName=password]').type(`${"test!1234"}`)
+      cy.get('[data-testid=register-invalid-form]').should('be.disabled')
+  
+      cy.get('input[formControlName=email]').type("user@test.com")
+      cy.get('input[formControlName=password]').clear()
+      cy.get('[data-testid=register-invalid-form]').should('be.disabled')
+
+      cy.get('input[formControlName=password]').type(`${"test!1234"}`)      
+      cy.get('input[formControlName=firstName]').clear()
+      cy.get('[data-testid=register-invalid-form]').should('be.disabled')
+
+    })
+  });
