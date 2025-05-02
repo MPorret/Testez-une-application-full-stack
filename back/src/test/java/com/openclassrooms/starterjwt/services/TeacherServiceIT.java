409dbc4ed6f3b8379ebb02f76f40e4c7ddc35658
MarineP <marine.porret@gmail.com>
2025-05-02 12:55:03 +0200

    ✅ Add integration tests for teacher service

---
+++ back/src/test/java/com/openclassrooms/starterjwt/services/TeacherServiceIT.java
@@ -0,0 +1,52 @@
+package com.openclassrooms.starterjwt.services;
+
+import com.openclassrooms.starterjwt.models.Teacher;
+import com.openclassrooms.starterjwt.repository.TeacherRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@SpringBootTest
+@Transactional
+public class TeacherServiceIT {
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    @Autowired
+    private TeacherService teacherService;
+
+    private Teacher teacher;
+
+    @BeforeEach
+    void init() {
+        Teacher teacher = new Teacher();
+        teacher.setFirstName("Test");
+        teacher.setLastName("User");
+        this.teacher = teacherRepository.save(teacher);
+    }
+
+    @Test
+    void testFindAll_shouldReturnAllTeachers (){
+        List<Teacher> allTeachers = teacherService.findAll();
+        assertEquals(3, allTeachers.size());
+    }
+
+    @Test
+    void testFindById_shouldReturnTeacher() {
+        Teacher teacherFound = teacherService.findById(teacher.getId());
+        assertEquals(teacherFound.getId(), teacher.getId());
+    }
+
+    @Test
+    void testFindById_shouldReturnNull() {
+        Teacher teacherFound = teacherService.findById(164L);
+        assertNull(teacherFound);
+    }
+}
